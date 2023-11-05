dda0eb167fc5241316beb0c628308ced4d83a601
sashasekirkina <alexasekirkina@gmail.com>
2023-11-05 22:08:55 +0700

    Configs added

---
+++ src/configs.py
@@ -0,0 +1,3 @@
+URL = "https://probe.fbrq.cloud/v1/send/"
+TOKEN = ("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3Mjg5OTU5ODUsImlzcyI6ImZhYnJpcXVlIiwibmFtZSI6Imh0dHBzOi8"
+         "vdC5tZS9yYXdzaWVyYSJ9.fBn8yoHo15C6Ai9CLwv4iQruzBpCD7y5kV1H8aE6ivE")
