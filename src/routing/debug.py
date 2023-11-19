cc381e68e5c5770a33fbbd6cac8e415e55deae4e
sashasekirkina <alexasekirkina@gmail.com>
2023-11-19 16:17:57 +0700

    Debug route updated

--- src/routing/debug.py
+++ src/routing/debug.py
@@ -7,4 +7,5 @@ router = APIRouter(prefix="/debug", tags=["debug"])
 
 @router.get('/messages', responses={404: {"message": "Not found"}})
 def get_messages():
-    ...
+    return data_manager.messages.get_all_messages()
+
